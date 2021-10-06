e5b8b78acc5158b0d0ca3de4a21136e4df0945b2
nidarg <gradin_g@yahoo.com>
2021-10-06 13:09:33 +0300

    fix url from first ex

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -2,7 +2,7 @@
 
 
 // this comes from netlify server which takes our function and execute it based on congig from netlify.toml file
-exports.hanler = async(event, context)=>{
+exports.handler = async(event, context)=>{
 	return{
 		statusCode:200,
 		body:'First Netlify Function'
