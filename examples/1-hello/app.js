fd86c7e011a718c19305acde7602ec8f251b143b
nidarg <gradin_g@yahoo.com>
2021-10-06 13:03:15 +0300

    fix url from first ex

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -2,8 +2,8 @@ const result = document.querySelector('.result')
 
 const fetchData = async()=>{
 	try{
-		// const {data} = axios.get('/.netlify/functions/1-hello')
-		const {data} = axios.get('/api/1-hello')
+		const {data} = axios.get('/.netlify/functions/1-hello')
+		// const {data} = axios.get('/api/1-hello')
 		result.textContent = data
 	}catch(error){
 		result.textContent = error.response.data
